ac5937d6fc2824b1bf0260f228f4f29c96267def
CarlosCadavez <cs.cadavez@gmail.com>
2021-12-12 21:16:31 -0300

    feat: wsdl config

--- src/main/java/br/com/cscdesenvolvimento/account/soap/WSConfig.java
+++ src/main/java/br/com/cscdesenvolvimento/account/soap/WSConfig.java
@@ -4,9 +4,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
@@ -20,5 +24,21 @@ public class WSConfig extends WsConfigurerAdapter {
         messageDispatcherServlet.setApplicationContext(applicationContext);
         messageDispatcherServlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean(messageDispatcherServlet, URL_MAPPING);
+    }
+
+    @Bean
+    public XsdSchema accountSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("account-info.xsd"));
+    }
+
+    @Bean(name = "accounts")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema accountSchema) {
+        DefaultWsdl11Definition defaultWsdl11Definition = new DefaultWsdl11Definition();
+        defaultWsdl11Definition.setPortTypeName("AccountPort");
+        defaultWsdl11Definition.setTargetNamespace("http://cscdesenvolvimento.com.br");
+        defaultWsdl11Definition.setLocationUri("/ws");
+        defaultWsdl11Definition.setSchema(accountSchema);
+
+        return defaultWsdl11Definition;
     }
 }
